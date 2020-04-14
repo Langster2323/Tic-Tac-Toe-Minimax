d2a9916a92c914dfd2905dcd23f6ad56cfe97e5e
Langster2323 <langahkeem@gmail.com>
2020-04-14 12:19:14 -0500

    Completed tic tac toe game with simple ai

--- index.js
+++ index.js
@@ -12,21 +12,83 @@ const winCombos = [
     [6, 4, 2],
 ]
 
-const cells = document.querySelectorAll('.cell')
+const cells = document.querySelectorAll('.cell');
 
 startGame();
 
 function startGame() {
     document.querySelector('.endgame').style.display = "none";
-    origBoard = Array.from(Array(9).keys())
-    for (let i = 0; i < cells.length; i ++) {
-        cells[i].innetText = ''
-        cells[i].style.removeProperty('background-color')
-        cells[i].addEventListener('click', turnClick, false)
+    origBoard = Array.from(Array(9).keys());
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].innetText = '';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click', turnClick, false);
     }
 }
 
 function turnClick(square) {
-    console.log(square.target.id);
-    
+    if(typeof origBoard[square.target.id] == 'number') {
+        turn(square.target.id, huPlayer);
+        if (!checkTie()) {
+            turn(bestSpot(), aiPlayer)
+        }
+    }
+}
+
+function turn(squareId, player) {
+    origBoard[squareId] = player;
+    document.getElementById(squareId). innerText = player;
+    let gameWon = checkWin(origBoard, player)
+    if (gameWon) {
+        gameOver(gameWon)
+    }
+}
+
+function checkWin(board, player) {
+    let plays = board.reduce((a, e, i) => 
+    (e === player) ? a.concat(i) : a, []) 
+    let gameWon = null
+    for (let [index, win] of winCombos.entries()) {
+        if (win.every(elem => plays.indexOf(elem) > -1)) {
+            gameWon = {index: index, player: player};
+            break;
+        }
+    }
+    return gameWon
+}
+
+function gameOver(gameWon) {
+    for (let index of winCombos[gameWon.index]) {
+        document.getElementById(index).style.backgroundColor = gameWon.player == huPlayer ? "blue" : "red"
+    }
+    for ( let i = 0; i < cells.length; i++) {
+        cells[i].removeEventListener('click', turnClick, false);
+    }
+
+    decalreWinner(gameWon.player == huPlayer ? "You win!" : "You lose!")
+}
+
+function decalreWinner(who) {
+    document.querySelector('.endgame').style.display = "block";
+    document.querySelector('.endgame .text').innerText = who;
+}
+
+function emptySqaures() {
+    return origBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+    return emptySqaures()[0];
+}
+
+function checkTie() {
+    if (emptySqaures().length == 0) {
+        for(let i = 0; i < cells.length; i++) {
+            cells[i].style.backgroundColor = 'green';
+            cells[i].removeEventListener('click', turnClick, false);
+        }
+        decalreWinner('Tie Game!');
+        return true;
+    }
+    return false;
 }
