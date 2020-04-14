5811da98ee588105e87544a67631e7240682dfaa
Langster2323 <langahkeem@gmail.com>
2020-04-14 12:44:02 -0500

    fixed error, now able to replay

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ const cells = document.querySelectorAll('.cell');
 startGame();
 
 function startGame() {
-    document.querySelector('.endgame').style.display = "none";
-    origBoard = Array.from(Array(9).keys());
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].innetText = '';
-        cells[i].style.removeProperty('background-color');
-        cells[i].addEventListener('click', turnClick, false);
-    }
+	document.querySelector(".endgame").style.display = "none";
+	origBoard = Array.from(Array(9).keys());
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].innerText = '';
+		cells[i].style.removeProperty('background-color');
+		cells[i].addEventListener('click', turnClick, false);
+	}
 }
 
 function turnClick(square) {
